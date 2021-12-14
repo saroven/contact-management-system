2fc1e55759056f7b8cbaa76eae137468cf7e1300
= <=>
2021-12-14 20:25:08 +0600

    validation complete

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -12,9 +12,13 @@ class SettingsController extends Controller
     }
     public function update(Request $request)
     {
-//        $request->validate([
-//            'appName' => 'required'
-//        ]);
+        $request->validate([
+            'appName' => 'required|string|min:0|max:15',
+            'appDescription' => 'required|string|min:0|max:125',
+            'appKeyword' => 'required|string|min:0|max:150',
+            'appLanguage' => 'required|string|min:0|max:5',
+            'userRegistration' => 'nullable|string|min:0|max:5',
+        ]);
         return $request->all();
     }
 }
--- resources/views/public/siteConfiguration.blade.php
+++ resources/views/public/siteConfiguration.blade.php
@@ -7,6 +7,11 @@
               </h3>
             </div>
             <!-- /.card-header -->
+            @if(Session::has('error'))
+              <x-Message type="error" :message="Session::get('error')"></x-Message>
+            @elseif(Session::has('success'))
+              <x-Message type="success" :message="Session::get('success')"></x-Message>
+            @endif
             <!-- form start -->
             <form role="form" method="post" action="{{ route('siteConfiguration') }}">
                 @csrf
@@ -15,50 +20,81 @@
                   <label for="name">Application Name</label>
                   <input
                     type="text"
-                    class="form-control"
+                    class="form-control @error('appName') is-invalid @enderror"
                     id="name"
                     name="appName"
+                    value="{{ old('appName') }}"
                     placeholder="Application Name"
                   />
+                    @error('appName')
+                      <span class="invalid-feedback" role="alert">
+                        {{ $message }}
+                      </span>
+                    @enderror
                 </div>
                 <div class="form-group">
                   <label for="description">App Description</label>
                   <input
                     type="text"
-                    class="form-control"
+                    class="form-control @error('appDescription') is-invalid @enderror"
                     id="appDescription"
-                    name="description"
+                    name="appDescription"
+                    value="{{ old('appDescription') }}"
                     placeholder="App description"
                   />
+                    @error('appDescription')
+                      <span class="invalid-feedback" role="alert">
+                        {{ $message }}
+                      </span>
+                    @enderror
                 </div>
                 <div class="form-group">
                   <label for="keyword">App Keyword</label>
                   <input
                     type="text"
-                    class="form-control"
+                    class="form-control @error('appKeyword') is-invalid @enderror"
                     id="keyword"
                     name="appKeyword"
+                    value="{{ old('appKeyword') }}"
                     placeholder="App keyword"
                   />
+                    @error('appKeyword')
+                      <span class="invalid-feedback" role="alert">
+                        {{ $message }}
+                      </span>
+                    @enderror
                 </div>
                 <div class="form-group">
                   <label for="language">App Language</label>
-                  <select name="appLanguage" id="language" class="form-control">
-                    <option value="en">English</option>
-                    <option value="bn">Bangla</option>
+                  <select
+                      name="appLanguage"
+                      id="language"
+                      class="form-control @error('appLanguage') is-invalid @enderror">
+                    <option value="en" @if(old('appLanguage') == 'en') selected @endif>English</option>
+                    <option value="bn" @if(old('appLanguage') == 'bn') selected @endif>Bangla</option>
                   </select>
+                    @error('appLanguage')
+                      <span class="invalid-feedback" role="alert">
+                        {{ $message }}
+                      </span>
+                    @enderror
                 </div>
                 <div class="custom-control custom-checkbox">
                   <input
-                    class="custom-control-input"
+                    class="custom-control-input @error('userRegistration') is-invalid @enderror"
                     type="checkbox"
                     id="registerStatus"
-                    checked=""
+                    @if(old('userRegistration')) checked="" @endif
                     name="userRegistration"
                   />
                   <label for="registerStatus" class="custom-control-label"
                     >Enable user registration</label
                   >
+                    @error('userRegistration')
+                      <span class="invalid-feedback" role="alert">
+                        {{ $message }}
+                      </span>
+                    @enderror
                 </div>
               </div>
               <!-- /.card-body -->
