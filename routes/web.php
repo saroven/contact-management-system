22b3c77d94f132c4e1ba4ce5fdec3c3afa200a32
= <=>
2022-01-25 00:43:58 +0600

    added routemiddleware to route page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use App\Http\Controllers\SettingsController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
-
+Route::middleware(['admin'])->group(function (){
+    Route::get('/dashboard', function (){
+        return 'Hello Admin';
+    });
+});
 Route::middleware(['auth'])->group(function (){ //route middleware group
     Route::get('/', [ContactController::class, 'showContact'])->name('home');
 
@@ -35,9 +39,6 @@ Route::middleware(['auth'])->group(function (){ //route middleware group
     Route::get('change-password', [UserController::class, 'changePassword'])->name('changePassword');
     Route::post('change-password', [UserController::class, 'updatePassword'])->name('updatePassword');
 
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->middleware(['auth'])->name('dashboard');
 
     Route::get('/profile', [\App\Http\Controllers\ProfileController::class, 'showProfilePage'])->middleware(['auth'])->name('profile');
     Route::post('/profile/update', [\App\Http\Controllers\ProfileController::class, 'updateProfile'])->middleware(['auth'])->name('profile.update');
@@ -47,5 +48,4 @@ Route::middleware(['auth'])->group(function (){ //route middleware group
         return redirect()->back();
     })->name('customLogout');
 });
-require __DIR__.'/auth.php';
 
