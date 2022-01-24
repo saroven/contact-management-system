b89fd35eb74b6111c74682ffc005d381fdfdf0e3
= <=>
2022-01-25 00:38:01 +0600

    register admin middleware

--- resources/views/public/layout/components/sidebar.blade.php
+++ resources/views/public/layout/components/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="home.blade.php" class="brand-link">
+        <a href="{{ route('home') }}" class="brand-link">
           <!-- <img
             src="dist/img/AdminLTELogo.png"
             alt="AdminLTE Logo"
