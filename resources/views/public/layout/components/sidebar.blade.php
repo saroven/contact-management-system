467e406e05390d9be014b571ef52523653af2a60
= <=>
2022-01-25 01:32:11 +0600

    removed sidebar user panel

--- resources/views/public/layout/components/sidebar.blade.php
+++ resources/views/public/layout/components/sidebar.blade.php
@@ -15,20 +15,6 @@
 
         <!-- Sidebar -->
         <div class="sidebar">
-          <!-- Sidebar user panel (optional) -->
-          <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-              <img
-                src="{{ asset('assets/img/user-profile/user2-160x160.jpg') }}"
-                class="img-circle elevation-2"
-                alt="User Image"
-              />
-            </div>
-            <div class="info">
-              <a href="#" class="d-block">Mohammad Shah Alam</a>
-            </div>
-          </div>
-
           <!-- Sidebar Menu -->
           <nav class="mt-2">
             <ul
